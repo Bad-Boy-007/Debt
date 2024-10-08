8ce10d68c9e18720a16f7dac263685ea3edee64c
mrdgm <mr.dgmironov@mail.ru>
2024-10-08 17:09:33 +0600

    add longOperation and test

---
+++ Backend/Gameserver/Commands/LongOperation.cs
@@ -0,0 +1,29 @@
+﻿using Gameserver.Interfaces;
+using Hwdtech;
+
+
+namespace Gameserver.Commands
+{
+    public class LongOperation : IStrategy
+    {
+        private readonly string _cmdName;
+        private readonly IUObject _target;
+
+        public LongOperation(string cmdName, IUObject target)
+        {
+            _cmdName = cmdName;
+            _target = target;
+        }
+
+        public object Execute(params object[] args)
+        {
+            var macroCommand = IoC.Resolve<Interfaces.ICommand>("Gameserver.MacroCommand.Create", _cmdName, _target);
+
+            var repeatCommand = IoC.Resolve<Interfaces.ICommand>("Gameserver.Command.Repeat", macroCommand);
+
+            var injectCommand = IoC.Resolve<Interfaces.ICommand>("Gameserver.Command.Inject", repeatCommand);
+
+            return injectCommand;
+        }
+    }
+}
